933848890fc4742a952e2140460853f53116771b
SrhAlvs <sssarahalves@gmail.com>
2025-05-08 16:46:06 -0300

    dever de casa passado no dia 24-04-2025

--- algoritmosETecnicasDeProgramacao/2025-04-24atividadeCasa1.cpp
+++ algoritmosETecnicasDeProgramacao/2025-04-24atividadeCasa1.cpp
@@ -3,15 +3,16 @@
 #define ANOATUAL 2025
 using namespace std;
 main () {
-	/*ENUNCIADO: Fa�a um programa que leia: idade e o nome de uma pessoa.
+	/*ENUNCIADO: Faca um programa que leia: idade e o nome de uma pessoa.
 	Escreva qual ano que ela nasceu.
 	Suponha que ela ja fez aniversario esse ano.*/
+	
 	string nomeUsuario;
 	int idadeUsuario, anoNascimento;
-
+	
 	cout<<"Insira seu nome: "<<endl; //mostrar na tela
 	cin>>nomeUsuario; //receber o nome do usuario
-	cout<<nomeUsuario<<", insira a sua idade: "<<endl; //mostrar na tela
+	cout<<nomeUsuario<<",supondo que ja tenha feito aniversario esse ano, insira a sua idade: "<<endl; //mostrar na tela
 	cin>>idadeUsuario;
 	anoNascimento = ANOATUAL - idadeUsuario;
 	cout<<nomeUsuario<<", voce nasceu no ano de "<<anoNascimento<<"."<<endl;
---
+++ algoritmosETecnicasDeProgramacao/2025-04-24atividadeCasa2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#define ANOPASSADO 2024
+using namespace std;
+main () {
+	/*ENUNCIADO: Faca um programa que leia: a idade e o nome de uma pessoa.
+	Escreva o ano que ela nasceu. 
+	Suponha que ela ainda nao fez aniversario esse ano.*/
+	
+	string nomeUsuario;
+	int idadeUsuario, anoNascimento;
+	
+	cout<<"Insira seu nome: "<<endl; //mostrar na tela
+	cin>>nomeUsuario; //receber o nome do usuario
+	cout<<nomeUsuario<<", supondo que ainda nao fez aniversario esse ano, insira a sua idade: "<<endl; //mostrar na tela
+	cin>>idadeUsuario;
+	anoNascimento = ANOPASSADO - idadeUsuario;
+	cout<<nomeUsuario<<", voce nasceu no ano de "<<anoNascimento<<"."<<endl;
+}

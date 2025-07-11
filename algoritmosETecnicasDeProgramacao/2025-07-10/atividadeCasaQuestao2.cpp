2b11e768f034af36c39ddc246f52607fa56af21d
Sarah Alves Soares do Nascimento <alves.soares@academico.ifg.edu.br>
2025-07-11 20:29:20 -0300

    aula LC do dia 2025-07-11

---
+++ algoritmosETecnicasDeProgramacao/2025-07-10/atividadeCasaQuestao2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <locale.h>
+using namespace std;
+int main () {
+	setlocale(LC_ALL, "Portuguese");
+	/*Alun@: Sarah Alves Soares do Nascimento
+	Questão 01:
+	Um professor está realizando uma atividade com seus alunos sobre progressão aritmética (PA). 
+	Sabendo que, em uma PA, a diferença entre o primeiro e o segundo termo (razão) é a mesma 
+	entre quaisquer dois termos consecutivos, faça um programa que:
+	1. Leia os dois primeiros termos da PA;
+	2. Leia as sequências fornecidas por 5 alunos (cada sequência deve ser verificada até que
+	um termo ultrapasse 90 ou esteja incorreto);
+	3. Imprima quantos alunos acertaram a sequência.
+	Observação: Um aluno erra a sequência se:
+	• Os valores não corresponderem à PA definida inicialmente;
+	• Um termo lido for maior que 90 (condição de parada).*/
+}
